02aa3ed254bde670d78c1f0bc4f23bc6ed5da1d0
kentaro.mimata <kentaro.mimata@rakus.co.jp>
2019-09-30 14:06:37 +0900

    Updates

---
+++ precache-manifest.baf541a155714eaf77ea8129faa977c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b8997beb1bf79250482b573b14326c",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "5879be7479e08cd47b04",
+    "url": "/Tic-Tac-Toe/static/css/main.2870ff06.chunk.css"
+  },
+  {
+    "revision": "1b54f433788fa4663907",
+    "url": "/Tic-Tac-Toe/static/js/2.ff3c778c.chunk.js"
+  },
+  {
+    "revision": "5879be7479e08cd47b04",
+    "url": "/Tic-Tac-Toe/static/js/main.35c7ff5f.chunk.js"
+  },
+  {
+    "revision": "5f185a476feae793e4b6",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.38dd14f6.js"
+  }
+]);
